3317a3d757c6599ef53650b1417828454c23eb0f
Grzegorz Czaplik <faflik@onet.pl>
2026-02-15 12:52:33 +0100

    wsparcie dla cliphist

--- init.lua
+++ init.lua
@@ -115,6 +115,17 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- moje zmniany ze schowkiem
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    local yanked = vim.fn.getreg '"'
+    if yanked ~= '' then
+      vim.fn.system('echo ' .. vim.fn.shellescape(yanked) .. ' | /home/japatu/.local/bin/cliphist stdin')
+    end
+  end,
+})
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
